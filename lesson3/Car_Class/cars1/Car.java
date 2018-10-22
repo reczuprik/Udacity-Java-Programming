0b5729060b53db404b39b0ded07db9b595fa2510
Your Name <you@example.com>
2018-10-22 21:01:48 +0200

    ADD lesson3.zip content

--- lesson3/Car_Class/cars1/Car.java
+++ lesson3/Car_Class/cars1/Car.java
@@ -1,17 +1,17 @@
-
 public class Car
 {
     private double milesDriven;
     private double gasInTank;
-    private Picture pic;
-    
-    
-    public void drive(double distance){
+
+    public void drive(double distance)
+    {
         milesDriven = milesDriven + distance;
-        pic.translate(distance,0);
     }
-    
-    public void addGas(double amount){
+
+    public void addGas(double amount)
+    {
         gasInTank = gasInTank + amount;
     }
+
+    // More methods...
 }
