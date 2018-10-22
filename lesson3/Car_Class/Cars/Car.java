bbb5ab0a35926fded4fbf6b2d7b6caa337352a8e
Your Name <you@example.com>
2018-10-22 20:52:04 +0200

    ADD drive and addGas method

--- lesson3/Car_Class/Cars/Car.java
+++ lesson3/Car_Class/Cars/Car.java
@@ -1,5 +1,14 @@
 
 public class Car
 {
-
+    private double milesDriven;
+    private double gasInTank;
+    
+    public void drive(double distance){
+        milesDriven = milesDriven + distance;
+    }
+    
+    public void addGas(double amount){
+        gasInTank = gasInTank + amount;
+    }
 }
