aebfc5613c4f310105aa2404a71bd11f6492043a
Your Name <you@example.com>
2018-10-23 12:32:51 +0200

    Update cars to recent version import classes from car3 project add javadoc note

--- lesson3/Cars/Car.java
+++ lesson3/Cars/Car.java
@@ -1,17 +1,58 @@
-
 public class Car
 {
     private double milesDriven;
     private double gasInTank;
     private Picture pic;
-    
-    
-    public void drive(double distance){
-        milesDriven = milesDriven + distance;
-        pic.translate(distance,0);
+    private double milesPerGallon;
+
+    public Car(double mpg) // Discussed later
+    {
+        milesDriven = 0;
+        gasInTank = 0;
+        milesPerGallon = mpg;
+        pic = new Picture("car.jpg");
+        pic.draw();
     }
     
-    public void addGas(double amount){
+    public Car(double mpg, String pictureFile) // Discussed later
+    {
+        milesDriven = 0;
+        gasInTank = 0;
+        milesPerGallon = mpg;
+        pic = new Picture(pictureFile);
+        pic.draw();
+    }
+    
+    public void drive(double distance)
+    {
+        milesDriven = milesDriven + distance;
+        
+        double gasConsumed = distance /milesPerGallon;
+        gasInTank=gasInTank-gasConsumed;
+        int pixelsPerMile = 10;
+        pic.translate(distance * pixelsPerMile, 0);
+    }
+  
+    /**
+     * 
+     *  Add gas  to the tank of this car
+     *  @param amount the amount of the gas to add
+    */
+    public void addGas(double amount)
+    {
         gasInTank = gasInTank + amount;
     }
+    
+    public double getMilesDriven()
+    {
+        return milesDriven;
+    }
+    
+    public double getGasInTank()
+    {
+        return gasInTank;
+    }
+    
+    // More methods ...
+
 }
