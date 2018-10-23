0ff16c2f8d7eb8970cce6d3fb2ecfbceefc12a73
Your Name <you@example.com>
2018-10-23 11:45:41 +0200

    Add getFriend accessor method

--- lesson3/Friends/Friends/Person.java
+++ lesson3/Friends/Friends/Person.java
@@ -8,10 +8,17 @@ public class Person
         name=aName;
         friends="";
     }
+    
+    public String getFriends()
+    {
+        return friends;
+    }
+    
     public void addFriend(Person friend)
     {
         friends= friends +  friend.name + " ";
     }
+    
     public void unFriend(Person nonFriend)
     {
         friends= friends.replace(nonFriend.name + " ","");
