39071a50274c91573baec4a75a7e8e3b609341e8
Your Name <you@example.com>
2019-01-08 19:50:22 +0100

    Final project scratch downloaded

---
+++ Course Assessment/Is X or Z/Is X or Z/WordUtilTester.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class WordUtilTester here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class WordUtilTester
+{
+ 
+   public static void main( )
+   {
+       WordUtil isxorz= new WordUtil();
+       boolean x=isxorz.isXOrZ("tegla");
+       System.out.println(x);
+
+   }
+
+ 
+}
